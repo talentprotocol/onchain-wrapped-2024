8d1710559a34bb7872821f85d2b4373e92156d08
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2024-12-19 16:30:42 +0000

    Store auth token in local storage on sign in

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import Back from "@/app/assets/icons/back.svg";
 import TalentLogo from "@/app/assets/images/logo.svg";
 import { Button } from "@/app/components/atoms";
 
+import { Suspense } from "react";
+import ReactQueryProvider from "./components/queryClientProvider";
 import "./globals.css";
 
 const dmMono = DM_Mono({ weight: ["500"], preload: false });
@@ -24,15 +26,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${dmMono.className} ${inter.className} antialiased flex flex-col items-center h-svh pt-2`}>
-        <header>
-          <nav className="grid grid-cols-3 items-center px-4">
-            <Button variant="ghost" className="w-fit">
-              <Image src={Back} alt="Back button" />
-            </Button>
-            <Image src={TalentLogo} alt="Talent Protocol logo" />
-          </nav>
-        </header>
-        <main className="w-full h-full flex flex-col items-center p-4 md:p-14 lg:p-28">{children}</main>
+        <ReactQueryProvider>
+          <Suspense>
+            <header>
+              <nav className="grid grid-cols-3 items-center px-4">
+                <Button variant="ghost" className="w-fit">
+                  <Image src={Back} alt="Back button" />
+                </Button>
+                <Image src={TalentLogo} alt="Talent Protocol logo" />
+              </nav>
+            </header>
+            <main className="w-full h-full flex flex-col items-center p-4 md:p-14 lg:p-28">{children}</main>
+          </Suspense>
+        </ReactQueryProvider>
       </body>
     </html>
   );
---
+++ app/login_callback/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const saveToken = (token: string) => {
+  localStorage.setItem("auth_token", token);
+};
+
+export default function Page() {
+  const router = useRouter();
+  const urlSearchParams = useSearchParams();
+  const pathname = usePathname();
+
+  useQuery({
+    queryKey: ["refresh_token", pathname, urlSearchParams],
+    queryFn: async () => {
+      const result = await fetch("/api/users", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          AUTHORIZATION: `Bearer ${urlSearchParams.get("auth_token") || ""}`
+        }
+      });
+
+      const data = await result.json();
+      const token = data?.authToken?.token;
+
+      if (!token) {
+        router.push("/");
+      }
+
+      saveToken(token);
+      router.push("/loading_wrapped");
+
+      return token;
+    },
+    enabled:
+      ["/login_callback"].includes(pathname) && urlSearchParams.has("auth_token") && !!urlSearchParams.get("auth_token")
+  });
+
+  return <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />;
+}
