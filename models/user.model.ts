dbf384bb0effb1d94690fbce9de0fef6a3920092
Rúben Sousa Dinis <Ruben.sousa.dinis@gmail.com>
2024-12-19 15:01:55 +0000

    Add endpoint to fetch user onchain wrapped (#8)

--- models/user.model.ts
+++ models/user.model.ts
@@ -14,4 +14,11 @@ export interface UserModel {
   loading_wallets_pnl: boolean;
   loading_wallets_transactions: boolean;
   loading_wallets_zora: boolean;
+  year_pnl: number;
+  year_transactions: number;
+  year_zora_posts: number;
+  year_zora_mints: number;
+  pnl_calculated_at: Date | undefined;
+  transactions_calculated_at: Date | undefined;
+  zora_calculated_at: Date | undefined;
 }
--- services/users/refreshUserWalletsData.ts
+++ services/users/refreshUserWalletsData.ts
@@ -1,49 +1,95 @@
 import { rollbarError } from "@/utils/rollbar/log";
 import { createSupabaseServerClient } from "@/utils/supabase/server";
-import { refreshWalletData } from "../wallets/refreshWalletData";
+import { getWalletPnL } from "../wallets/fetchWalletPnl";
+import { getWalletTransactionsCount } from "../wallets/fetchWalletTransactions";
+import { fetchZoraPostsAndMints } from "../wallets/fetchZoraPostsAndMints";
+
+interface WalletAddress {
+  address: string;
+}
 
 const supabase = await createSupabaseServerClient();
 
 export async function refreshUserWalletsData(userId: number) {
-  const { data, error: getWalletsError } = await supabase.from("wallets").select("address").eq("user_id", userId);
+  const { data: wallets, error: getWalletsError } = await supabase
+    .from("wallets")
+    .select("address")
+    .eq("user_id", userId);
 
   if (getWalletsError) {
     rollbarError("Unable to get user wallets", getWalletsError);
     return;
   }
 
-  let walletsYearPnL = 0;
-  let walletsYearTransactions = 0;
-  let walletsZoraPosts = 0;
-  let walletsZoraMints = 0;
+  await fetchTransactions(userId, wallets);
+  await fetchPNL(userId, wallets);
+  await fetchFromZora(userId, wallets);
+}
 
-  for (const wallet of data) {
-    const walletsData = await refreshWalletData(wallet.address);
-    walletsYearPnL += walletsData.walletYearPnL;
-    walletsYearTransactions += walletsData.walletYearTransactions;
-    walletsZoraPosts += walletsData.zoraPosts;
-    walletsZoraMints += walletsData.zoraMints;
+async function fetchPNL(userId: number, wallets: WalletAddress[]) {
+  let walletsYearPNL = 0;
+
+  for (const wallet of wallets) {
+    const walletPNL = await getWalletPnL(wallet.address);
+    walletsYearPNL += walletPNL;
   }
-
-  const currentDate = new Date();
 
   const { error } = await supabase
     .from("users")
     .update({
       loading_wallets_pnl: false,
-      loading_wallets_transactions: false,
-      loading_wallets_zora: false,
-      year_pnl: walletsYearPnL,
-      year_transactions: walletsYearTransactions,
-      year_zora_posts: walletsZoraPosts,
-      year_zora_mints: walletsZoraMints,
-      pnl_calculated_at: currentDate,
-      transactions_calculated_at: currentDate,
-      zora_calculated_at: currentDate
+      year_pnl: walletsYearPNL,
+      pnl_calculated_at: new Date()
     })
     .eq("id", userId);
 
   if (error) {
-    rollbarError("Unable to update user loading state", error);
+    rollbarError(`Unable to update user ${userId} pnl`, error);
+  }
+}
+
+async function fetchTransactions(userId: number, wallets: WalletAddress[]) {
+  let walletsYearTransactions = 0;
+
+  for (const wallet of wallets) {
+    const walletTransactions = await getWalletTransactionsCount(wallet.address);
+    walletsYearTransactions += walletTransactions;
+  }
+
+  const { error } = await supabase
+    .from("users")
+    .update({
+      loading_wallets_transactions: false,
+      year_transactions: walletsYearTransactions,
+      transactions_calculated_at: new Date()
+    })
+    .eq("id", userId);
+
+  if (error) {
+    rollbarError(`Unable to update user ${userId} transactions`, error);
+  }
+}
+
+async function fetchFromZora(userId: number, wallets: WalletAddress[]) {
+  let walletsZoraPosts = 0;
+  let walletsZoraMints = 0;
+  for (const wallet of wallets) {
+    const { zoraPosts, zoraMints } = await fetchZoraPostsAndMints(wallet.address);
+    walletsZoraPosts += zoraPosts;
+    walletsZoraMints += zoraMints;
+  }
+
+  const { error } = await supabase
+    .from("users")
+    .update({
+      loading_wallets_zora: false,
+      year_zora_posts: walletsZoraPosts,
+      year_zora_mints: walletsZoraMints,
+      zora_calculated_at: new Date()
+    })
+    .eq("id", userId);
+
+  if (error) {
+    rollbarError(`Unable to update user ${userId} zora mints and posts`, error);
   }
 }
