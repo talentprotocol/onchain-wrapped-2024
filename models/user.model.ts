ea62a737d3333d942d5c5505a84f7cbaab08ba2a
Rúben Sousa Dinis <Ruben.sousa.dinis@gmail.com>
2024-12-20 17:41:22 +0000

    Add transactions count by chain (#15)

--- models/user.model.ts
+++ models/user.model.ts
@@ -17,33 +17,14 @@ export interface UserModel {
   loading_wallets_zora: boolean;
   year_pnl: number;
   year_transactions: number;
+  year_base_transactions: number;
+  year_bsc_transactions: number;
+  year_ethereum_transactions: number;
+  year_optimism_transactions: number;
+  year_arbitrum_transactions: number;
   year_zora_posts: number;
   year_zora_mints: number;
   pnl_calculated_at: Date | undefined;
   transactions_calculated_at: Date | undefined;
   zora_calculated_at: Date | undefined;
-}
-
-export interface UserClientModel {
-  id: number;
-  talentId: number;
-  builderScore: number;
-  credentialsCount: number;
-  githubContributions: number;
-  ens: string;
-  calculatedAt: Date | undefined;
-  onchainSince: Date | undefined;
-  wallets: WalletModel[];
-  baseTestnetContractsDeployed: number;
-  baseMainnetContractsDeployed: number;
-  loadingWalletsPnl: boolean;
-  loadingWalletsTransactions: boolean;
-  loadingWalletsZora: boolean;
-  yearPnL: number;
-  yearTransactions: number;
-  yearZoraPosts: number;
-  yearZoraMints: number;
-  pnlCalculatedAt: Date | undefined;
-  transactionsCalculatedAt: Date | undefined;
-  zoraCalculatedAt: Date | undefined;
 }
--- services/wallets/fetchWalletTransactions.ts
+++ services/wallets/fetchWalletTransactions.ts
@@ -2,8 +2,6 @@ import { rollbarError } from "@/utils/rollbar/log";
 import { createSupabaseServerClient } from "@/utils/supabase/server";
 import { getWalletTransactions } from "@/utils/zerion/client";
 
-const BEGINNING_OF_YEAR_TIMESTAMP = 1704067200;
-const END_OF_YEAR_TIMESTAMP = 1735689599;
 const CHAINS = "ethereum,base,optimism,binance-smart-chain,arbitrum";
 const CURRENCY = "usd";
 
@@ -11,20 +9,52 @@ const supabase = await createSupabaseServerClient();
 
 export async function getWalletTransactionsCount(wallet: string) {
   let transactionsCount = 0;
+  let bscTransactionsCount = 0;
+  let arbitrumTransactionsCount = 0;
+  let ethereumTransactionsCount = 0;
+  let baseTransactionsCount = 0;
+  let optimismTransactionsCount = 0;
   let nextPageUrl = undefined;
 
   do {
-    const wallettransactionsResponse = await getWalletTransactions(wallet, CHAINS, CURRENCY, nextPageUrl);
+    const walletTransactionsResponse = await getWalletTransactions(wallet, CHAINS, CURRENCY, nextPageUrl);
 
-    nextPageUrl = wallettransactionsResponse?.links?.next;
+    nextPageUrl = walletTransactionsResponse?.links?.next;
 
-    transactionsCount += wallettransactionsResponse.data?.length || 0;
+    const transactions = walletTransactionsResponse.data || [];
+
+    transactionsCount += transactions.length;
+
+    for (const transaction of transactions) {
+      switch (transaction.relationships.chain.data.id) {
+        case "ethereum":
+          ethereumTransactionsCount += 1;
+          break;
+        case "base":
+          baseTransactionsCount += 1;
+          break;
+        case "optimism":
+          optimismTransactionsCount += 1;
+          break;
+        case "binance-smart-chain":
+          bscTransactionsCount += 1;
+          break;
+        case "arbitrum":
+          arbitrumTransactionsCount += 1;
+          break;
+      }
+    }
   } while (nextPageUrl);
 
   const { error } = await supabase
     .from("wallets")
     .update({
-      year_transactions: transactionsCount
+      year_transactions: transactionsCount,
+      bsc_transactions: bscTransactionsCount,
+      ethereum_transactions: ethereumTransactionsCount,
+      base_transactions: baseTransactionsCount,
+      arbitrum_transactions: arbitrumTransactionsCount,
+      optimism_transactions: optimismTransactionsCount
     })
     .eq("address", wallet);
 
@@ -32,5 +62,12 @@ export async function getWalletTransactionsCount(wallet: string) {
     rollbarError(`Unable to update wallet ${wallet} transactions`, error);
   }
 
-  return transactionsCount;
+  return {
+    transactionsCount,
+    ethereumTransactionsCount,
+    baseTransactionsCount,
+    optimismTransactionsCount,
+    bscTransactionsCount,
+    arbitrumTransactionsCount
+  };
 }
